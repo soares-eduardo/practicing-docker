8280f463d5b662d9b8fcb451f25bc6a8bcd3b135
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-23 14:46:53 -0300

    Deleted interfaces package.

--- InventoryMs/src/main/java/com/inventoryms/InventoryMs/InventoryController.java
+++ InventoryMs/src/main/java/com/inventoryms/InventoryMs/InventoryController.java
@@ -1,4 +1,4 @@
-package com.inventoryms.InventoryMs.interfaces;
+package com.inventoryms.InventoryMs;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
