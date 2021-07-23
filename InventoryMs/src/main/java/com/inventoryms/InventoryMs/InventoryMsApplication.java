9d1397476f73975481542a1cc98315787c60da1f
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-23 14:41:30 -0300

    Added Live Reload by Devtools dependency

--- InventoryMs/src/main/java/com/inventoryms/InventoryMs/InventoryMsApplication.java
+++ InventoryMs/src/main/java/com/inventoryms/InventoryMs/InventoryMsApplication.java
@@ -9,5 +9,4 @@ public class InventoryMsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryMsApplication.class, args);
 	}
-
 }
--- InventoryMs/src/main/java/com/inventoryms/InventoryMs/interfaces/InventoryController.java
+++ InventoryMs/src/main/java/com/inventoryms/InventoryMs/interfaces/InventoryController.java
@@ -10,7 +10,9 @@ public class InventoryController {
 
     @GetMapping
     public String getInventory() {
-        System.out.println("InventoryMS is working");
-        return "Inventory MS is working.";
+        System.out.println("1 - InventoryMS is working.");
+        System.out.println("2 - Now, it's been changed.");
+        System.out.println("3 - Live Reload's joined the gang.");
+        return "Inventory MS is working but with some changes and live reloading.";
     }
 }
